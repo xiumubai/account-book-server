a42f7981eb94a41e946310cbda73d2b2948d98a2
xiumubai <1547702880@qq.com>
2022-10-17 15:29:05 +0800

    feat: post & get 请求

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,28 @@
 'use strict';
-
 const { Controller } = require('egg');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, janney';
+    const { id } = ctx.query;
+    ctx.body = id;
+  }
+  async user() {
+    const { ctx } = this;
+    const { id } = ctx.params;
+    const { name, slogen } = await ctx.service.home.user();
+    ctx.body = {
+      id,
+      name,
+      slogen,
+    };
+  }
+  async add() {
+    const { ctx } = this;
+    const { title } = ctx.request.body;
+    ctx.body = {
+      title,
+    };
   }
 }
 
